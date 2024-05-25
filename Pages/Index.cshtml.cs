cfdc80ea7ae742a6e8c6372979bdb8596f351b25
DREXCC123 <dmacaspac@csmbataan.ph.education>
2024-05-25 22:13:42 +0800

    filtering

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -14,138 +14,184 @@ namespace WebApplicationDrexlerMacaspac.Pages
 
         public List<Car> Cars { get; set; }
 
+        [BindProperty]
+        public SearchParameters? SearchParams { get; set; }
 
-        public void OnGet(string? sortBy = null, string? sortAsc = "true")
+
+        public void OnGet(string? keyword = "", string? searchBy = "", string? sortBy = null, string? sortAsc = "true")
         {
-            List<Car>? Cars = new List<Car>()
+            if (SearchParams == null)
             {
-                new Car () { 
-                    Make = "Toyota", 
-                    Model = "Camry", 
-                    Year = 2022, 
+                SearchParams = new SearchParameters()
+                {
+                    SortBy = sortBy,
+                    SortAsc = sortAsc == "true",
+                    SearchBy = searchBy,
+                    Keyword = keyword
+                };
+            }
+            
+                List<Car>? Cars = new List<Car>()
+            {
+                new Car () {
+                    Make = "Toyota",
+                    Model = "Camry",
+                    Year = 2022,
                     Color = "Red" },
 
-                new Car () { 
-                    Make = "Honda", 
-                    Model = "Civic", 
-                    Year = 2021, 
+                new Car () {
+                    Make = "Honda",
+                    Model = "Civic",
+                    Year = 2021,
                     Color = "Black" },
 
-                new Car () { 
-                    Make = "Ford", 
-                    Model = "Mustang", 
-                    Year = 2020, 
+                new Car () {
+                    Make = "Ford",
+                    Model = "Mustang",
+                    Year = 2020,
                     Color = "Blue" },
 
-                new Car () { 
-                    Make = "Chevrolet", 
-                    Model = "Corvette", 
-                    Year = 2019, 
+                new Car () {
+                    Make = "Chevrolet",
+                    Model = "Corvette",
+                    Year = 2019,
                     Color = "Yellow" },
 
-                new Car () { 
-                    Make = "BMW", 
-                    Model = "3 Series", 
-                    Year = 2018, 
+                new Car () {
+                    Make = "BMW",
+                    Model = "3 Series",
+                    Year = 2018,
                     Color = "White" },
 
-                new Car () { 
-                    Make = "Mercedes-Benz", 
-                    Model = "C-Class", 
-                    Year = 2017, 
+                new Car () {
+                    Make = "Mercedes-Benz",
+                    Model = "C-Class",
+                    Year = 2017,
                     Color = "Silver" },
 
-                new Car () { 
+                new Car () {
                     Make = "Audi",
-                    Model = "A4", 
-                    Year = 2016, 
+                    Model = "A4",
+                    Year = 2016,
                     Color = "Gray" },
 
-                new Car () { 
-                    Make = "Nissan", 
-                    Model = "Altima", 
-                    Year = 2015, 
+                new Car () {
+                    Make = "Nissan",
+                    Model = "Altima",
+                    Year = 2015,
                     Color = "Burgundy" },
 
-                new Car () { 
+                new Car () {
                     Make = "Subaru",
-                    Model = "Outback", 
+                    Model = "Outback",
                     Year = 2014,
                     Color = "Green" },
 
-                new Car () { 
+                new Car () {
                     Make = "Hyundai",
                     Model = "Sonata",
                     Year = 2013,
                     Color = "Blue" },
 
-                new Car () { 
+                new Car () {
                     Make = "Kia",
-                    Model = "Optima", 
+                    Model = "Optima",
                     Year = 2012,
                     Color = "Red" },
 
-                new Car () { 
-                    Make = "Volkswagen", 
+                new Car () {
+                    Make = "Volkswagen",
                     Model = "Jetta",
-                    Year = 2011, 
+                    Year = 2011,
                     Color = "Black" },
 
-                new Car () { 
-                    Make = "Mazda", 
-                    Model = "3", 
+                new Car () {
+                    Make = "Mazda",
+                    Model = "3",
                     Year = 2010,
                     Color = "Blue" },
 
                 new Car () {
-                    Make = "Lexus", 
+                    Make = "Lexus",
                     Model = "IS",
-                    Year = 2009, 
+                    Year = 2009,
                     Color = "White" },
 
-                new Car () { 
-                    Make = "Acura", 
-                    Model = "TL", 
-                    Year = 2008, 
+                new Car () {
+                    Make = "Acura",
+                    Model = "TL",
+                    Year = 2008,
                     Color = "Gray" }
             };
 
 
-            if (sortBy == null || sortAsc == null)
-            { 
-                this.Cars = Cars; 
-                return; }
+                if (!string.IsNullOrEmpty(SearchParams.SearchBy) && !string.IsNullOrEmpty(SearchParams.Keyword))
+                {
+                    if (SearchParams.SearchBy.ToLower() == "make")
+                    {
+                        Cars = Cars.Where(a => a.Make != null && a.Make.ToLower().Contains(SearchParams.Keyword.ToLower())).ToList();
+                    }
+                    else if (SearchParams.SearchBy.ToLower() == "model")
+                    {
+                        Cars = Cars.Where(a => a.Model != null && a.Model.ToLower().Contains(SearchParams.Keyword.ToLower())).ToList();
+                    }
+                    else if (SearchParams.SearchBy.ToLower() == "color")
+                    {
+                        Cars = Cars.Where(a => a.Color != null && a.Color.ToLower().Contains(SearchParams.Keyword.ToLower())).ToList();
+                    }
+                    else if (SearchParams.SearchBy.ToLower() == "year")
+                    {
+                        Cars = Cars.Where(a => a.Year.ToString().Contains(SearchParams.Keyword)).ToList();
+                    }
+                }
+                else if ((string.IsNullOrEmpty(SearchParams.SearchBy) || SearchParams.SearchBy == "") && !string.IsNullOrEmpty(SearchParams.Keyword))
+                {
+                    Cars = Cars.Where(a => a.Make != null && a.Make.ToLower().Contains(SearchParams.Keyword.ToLower())).ToList();
+                }
 
-            if (sortBy!.ToLower() == "make" && sortAsc!.ToLower() == "true")
-            { 
-                this.Cars = Cars.OrderBy(a => a.Make).ToList(); }
-            else if (sortBy!.ToLower() == "make" && sortAsc!.ToLower() == "false")
-            { 
-                this.Cars = Cars.OrderByDescending(a => a.Make).ToList(); }
-            else if (sortBy!.ToLower() == "model" && sortAsc!.ToLower() == "true")
-            { 
-                this.Cars = Cars.OrderBy(a => a.Model).ToList(); }
-            else if (sortBy!.ToLower() == "model" && sortAsc!.ToLower() == "false")
-            {
-                this.Cars = Cars.OrderByDescending(a => a.Model).ToList(); }
-            else if (sortBy!.ToLower() == "year" && sortAsc!.ToLower() == "true")
-            {
-                this.Cars = Cars.OrderBy(a => a.Year).ToList(); }
-            else if (sortBy!.ToLower() == "year" && sortAsc!.ToLower() == "false")
-            { 
-                this.Cars = Cars.OrderByDescending(a => a.Year).ToList(); }
-            else if (sortBy!.ToLower() == "color" && sortAsc!.ToLower() == "true")
-            { 
-                this.Cars = Cars.OrderBy(a => a.Color).ToList(); }
-            else if (sortBy!.ToLower() == "color" && sortAsc!.ToLower() == "false")
-            { 
-                this.Cars = Cars.OrderByDescending(a => a.Color).ToList();
+                if (SearchParams.SortBy == null || SearchParams.SortAsc == null)
+                {
+                    this.Cars = Cars;
+                    return;
+                }
+
+                if (SearchParams.SortBy.ToLower() == "make" && SearchParams.SortAsc == true)
+                {
+                    this.Cars = Cars.OrderBy(a => a.Make).ToList();
+                }
+                else if (SearchParams.SortBy.ToLower() == "make" && SearchParams.SortAsc == false)
+                {
+                    this.Cars = Cars.OrderByDescending(a => a.Make).ToList();
+                }
+                else if (SearchParams.SortBy.ToLower() == "model" && SearchParams.SortAsc == true)
+                {
+                    this.Cars = Cars.OrderBy(a => a.Model).ToList();
+                }
+                else if (SearchParams.SortBy.ToLower() == "model" && SearchParams.SortAsc == false)
+                {
+                    this.Cars = Cars.OrderByDescending(a => a.Model).ToList();
+                }
+                else if (SearchParams.SortBy.ToLower() == "year" && SearchParams.SortAsc == true)
+                {
+                    this.Cars = Cars.OrderBy(a => a.Year).ToList();
+                }
+                else if (SearchParams.SortBy.ToLower() == "year" && SearchParams.SortAsc == false)
+                {
+                    this.Cars = Cars.OrderByDescending(a => a.Year).ToList();
+                }
+                else if (SearchParams.SortBy.ToLower() == "color" && SearchParams.SortAsc == true)
+                {
+                    this.Cars = Cars.OrderBy(a => a.Color).ToList();
+                }
+                else if (SearchParams.SortBy.ToLower() == "color" && SearchParams.SortAsc == false)
+                {
+                    this.Cars = Cars.OrderByDescending(a => a.Color).ToList();
+                }
+                else
+                {
+                    this.Cars = Cars;
+                }
             }
-            else 
-            { 
-                this.Cars = Cars;
-            }
-        }
 
         public class Car
         {
@@ -154,5 +200,14 @@ namespace WebApplicationDrexlerMacaspac.Pages
             public int Year { get; set; }
             public string Color { get; set; }
         }
+
+        public class SearchParameters
+        {
+            public string? SearchBy { get; set; }
+            public string? Keyword { get; set; } 
+            public string? SortBy { get; set; }
+            public bool? SortAsc { get; set; }
+        }
+
     }
 }
